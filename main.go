67c278c933f850e12238613b21d4b92b145733f1
Chris Dial <dial.chris@gmail.com>
2022-01-23 08:35:54 -0800

    go fmt, some general cleanup

--- main.go
+++ main.go
@@ -1,15 +1,13 @@
 package main
 
 import (
-	"gopkg.in/yaml.v2"
 	"fmt"
+	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"log"
 	"net/url"
 	"os"
 	"os/exec"
-//	"strconv"
-//	"strings"
 	"sync"
 	"time"
 )
@@ -23,7 +21,7 @@ var endMiner, endChkBlk time.Time
 var user, pass, gpu, globalUnits, localUnits, GPUID, walletaddr string
 var u *url.URL
 
-/* DynMiner2.exe args:
+/* DynMiner2.exe args for reference:
 -mode [solo|stratum]
 -server <rpc server URL or stratum IP>
 -port <rpc port>  [only used for stratum]
@@ -33,36 +31,22 @@ var u *url.URL
 -wallet <wallet address>   [only used for solo]
 -miner <miner params>
 
-printf("<miner params> format:\n");
-    printf("  [CPU|GPU],<cores or compute units>[<work size>,<platform id>,<device id>[,<loops>]]\n");
-    printf("  <work size>, <platform id> and <device id> are not required for CPU\n");
+<miner params> format:
+[CPU|GPU],<cores or compute units>[<work size>,<platform id>,<device id>[,<loops>]]
+<work size>, <platform id> and <device id> are not required for CPU
 
-    -hiveos [0|1]   [optional, if 1 will format output for hiveos]
+-hiveos [0|1]   [optional, if 1 will format output for hiveos]
 -statrpcurl <URL to send stats to> [optional]
 -minername <display name of miner> [required with statrpcurl]
 */
 
-/* 
-For my initial purposes... let's read some of these args from a file, things that will not change between my miners:
--server 
--user 
--pass
--statrpcurl
--respawn time
-
-And we will ONLY take the following args:
--miner (eg, like GPU,25600,0,0)
--minername
-*/
-
-
 type conf struct {
-	NodeUrl             string  `yaml:"NodeUrl"`
-	NodeUser           string  `yaml:"NodeUser"`
-	NodePass           string  `yaml:"NodePass"`
-	WalletAddr           string  `yaml:"WalletAddr"`
-	StatRpcUrl             string  `yaml:"StatRpcUrl"`
-	RespawnSeconds int     `yaml:"RespawnSeconds"`
+	NodeUrl        string `yaml:"NodeUrl"`
+	NodeUser       string `yaml:"NodeUser"`
+	NodePass       string `yaml:"NodePass"`
+	WalletAddr     string `yaml:"WalletAddr"`
+	StatRpcUrl     string `yaml:"StatRpcUrl"`
+	RespawnSeconds int    `yaml:"RespawnSeconds"`
 }
 
 func (c *conf) getConf() *conf {
@@ -85,16 +69,12 @@ func (c *conf) getConf() *conf {
 
 var c conf
 
-
-
-
-/* NOTE: THIS VERSION IS ONLY FOR SOLO AND DOES NOT SUPPORT THE HIVE STUFF */
-
 func main() {
 	var args = os.Args[1:]
-    c.getConf() 
+	c.getConf()
 	if len(args) != 2 {
 		fmt.Fprintf(os.Stderr, "Usage: %s <minerargs> <minername>\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "Note: This does NOT yet support pool mining or HIVE options\n")
 		fmt.Fprintln(os.Stderr, "")
 		fmt.Fprintln(os.Stderr, "Example:")
 		fmt.Fprintf(os.Stderr, "    %s GPU,25600,64,0,0 MyMiner\n", os.Args[0])
@@ -142,13 +122,15 @@ func startMiner() {
 
 	endMiner = now.Add(ttl)
 
-	mineCmd = exec.Command("DynMiner2.exe", "-mode","solo","-server",c.NodeUrl, "-user",c.NodeUser,"-pass", c.NodePass, "-wallet", c.WalletAddr, "-miner", minerArgs, "-statrpcurl", c.StatRpcUrl, "-minername", minerName)
+	if len(c.StatRpcUrl) > 0 {
+		mineCmd = exec.Command("DynMiner2.exe", "-mode", "solo", "-server", c.NodeUrl, "-user", c.NodeUser, "-pass", c.NodePass, "-wallet", c.WalletAddr, "-miner", minerArgs, "-statrpcurl", c.StatRpcUrl, "-minername", minerName)
+	} else {
+		mineCmd = exec.Command("DynMiner2.exe", "-mode", "solo", "-server", c.NodeUrl, "-user", c.NodeUser, "-pass", c.NodePass, "-wallet", c.WalletAddr, "-miner", minerArgs)
+	}
 
-    log.Printf("Executing %q - will end at %s", mineCmd.String(), endMiner)
+	log.Printf("Executing %q - will end at %s", mineCmd.String(), endMiner)
 	time.Sleep(time.Second * 1)
 	mineCmd.Stdout = os.Stdout
 	mineCmd.Stdout = os.Stdout
 	mineCmd.Start()
 }
-
-
